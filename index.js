59d814e6d1093ab67a22d30120aeac273f439054
Matt Crecraft <mcrecraft@gmail.com>
2022-05-15 14:02:05 -0500

    Cleaned up JS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function displayPlan() {
   const step1 = document.querySelector("#step-1")
-  step1.textContent = "Collect underpants.";
+  step1.textContent = "Collect underpants."
+
   const step2 = document.querySelector("#step-2")
   step2.textContent = "???"
+
   const step3 = document.querySelector("#step-3")
   step3.textContent = "Profit!"
 
@@ -15,13 +17,3 @@ const button = document.querySelector("button")
 button.addEventListener("submit", event => {
   displayPlan()
 })
-
-
-
-
-  // < h1 > Our 3 - Step Plan:</h1 >
-  //   <ol>
-  //     <li id="step-1"></li>
-  //     <li id="step-2"></li>
-  //     <li id="step-3"></li>
-  //   </ol>
